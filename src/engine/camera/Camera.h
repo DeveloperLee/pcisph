a302d0dbd564d738dcfe588f12597fefd72395ae
DixuanYang <dixuan_yang@brown.edu>
2016-04-18 15:55:08 -0400

    camera finished

--- src/engine/camera/Camera.h
+++ src/engine/camera/Camera.h
@@ -1,50 +1,54 @@
-#ifndef __DX_PCISPH_FLUID_SIMULATOR_STD__
-#define __DX_PCISPH_FLUID_SIMULATOR_STD__
+#pragma once
 
 #include <utils/STD.h>
 #include <nanogui/glutil.h>
+
+
+namespace cs224 {
 
 class Camera {
 public:
 	Camera();
 	~Camera();
 
-	void setResolution();
-	void setPosition();
-	void setTarget();
-	void setUpVector();
-	void setFov();
-	void setNear();
-	void setFar();
+	void setResolution(const nanogui::Vector2i &resolution);
+	void setPosition(const nanogui::Vector3f &position);
+	void setTarget(const nanogui::Vector3f &target);
+	void setUpVector(const nanogui::Vector3f &up);
+	void setFov(float fov);
+	void setNear(float near);
+	void setFar(float far);
 
-	const Vector2i &resolution() const;
-	const Vector3f &position() const;
-	const Vector3f &target() const;
-	const Vector3f &up() const;
+	const nanogui::Vector2i &resolution() const;
+	const nanogui::Vector3f &position() const;
+	const nanogui::Vector3f &target() const;
+	const nanogui::Vector3f &up() const;
 
 	float fov() const;
 	float near() const;
 	float far() const;
 
-	Matrix4f viewMatrix() const;
-	Matrix4f projectionMatrix() const;
+	nanogui::Matrix4f viewMatrix() const;
+	nanogui::Matrix4f projectionMatrix() const;
 
-	bool mouseMotionEvent(const Vector2i &p, const Vector2i &rel, int button, int modifiers);
-	bool mouseButtonEvent(const Vector2i &p, int button, bool down, int modifiers);
-	bool scrollEvent(const Vector2i &p, const Vector2f &rel);
+	bool mouseMotionEvent(const nanogui::Vector2i &p, const nanogui::Vector2i &rel, int button, int modifiers);
+	bool mouseButtonEvent(const nanogui::Vector2i &p, int button, bool down, int modifiers);
+	bool scrollEvent(const nanogui::Vector2i &p, const nanogui::Vector2f &rel);
 
 private:
-	Vector2i m_resolution;
-	Vector3f m_position;
-	Vector3f m_target;
-	Vector3f m_up;
+	nanogui::Vector2i m_resolution;
+	nanogui::Vector3f m_position;
+	nanogui::Vector3f m_target;
+	nanogui::Vector3f m_up;
 	float m_fov;
 	float m_near;
 	float m_far;
+
+	nanogui::Arcball m_arcball;
+	bool m_leftButton = false;
 };
 
-#endif
-
+} // namespace cs224
 
 
 
